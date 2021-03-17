9e93a25403ac1da83418b388d4149341abd8e6b2
unknown <akdl911215@naver.com>
2021-03-17 09:59:59 +0900

    articleService, BoardService API 추가 및 수정

--- demo/src/main/java/com/example/demo/bbs/service/ArticleService.java
+++ demo/src/main/java/com/example/demo/bbs/service/ArticleService.java
@@ -1,5 +1,20 @@
 package com.example.demo.bbs.service;
 
-public interface ArticleService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Example;
+
+import com.example.demo.bbs.domain.Article;
+
+
+public interface ArticleService {
+	public Optional<Article> findOne(Article t);
+	public long count();
+	public boolean existsById(Long id);
+	public <Article> Article save(Article entity);
+	public <Article> Optional<Article> findOne(Example<Article> example);
+	public List<Article> findAllById(Iterable<Long> ids);
+	public void deleteById(Long id);
+	public Article getOne(Long id);
 }
