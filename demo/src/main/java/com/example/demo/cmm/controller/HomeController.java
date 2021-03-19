29529a5e52585b6cc4ab54e07d67bec2fa03348a
unknown <akdl911215@naver.com>
2021-03-19 13:08:10 +0900

    더미 작업

--- demo/src/main/java/com/example/demo/cmm/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/cmm/controller/HomeController.java
@@ -1,5 +1,18 @@
 package com.example.demo.cmm.controller;
 
-public class HomeController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.cmm.util.Proxy;
+
+@RestController
+public class HomeController extends Proxy{
+	
+	@GetMapping("/")
+	public String init() {
+		print.accept("==========================================");
+		print.accept("=============Boot Initializaed============");
+		print.accept("==========================================");
+		return "Hello React";
+	}
 }
